2c73a015bad7f612fb4037234be74d4500b66a54
ADizanzo <augustodizanzo@gmail.com>
2024-02-22 17:09:18 -0300

    agrego iconos y actualizo lógica para eliminar reserva (funciona)

--- app/src/main/java/com/example/tortupadel/MisReservasActivity.java
+++ app/src/main/java/com/example/tortupadel/MisReservasActivity.java
@@ -1,16 +1,11 @@
 package com.example.tortupadel;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.SQLException;
@@ -18,7 +13,7 @@ import java.util.List;
 
 public class MisReservasActivity extends AppCompatActivity {
     ListView listView;
-    ArrayAdapter<String> adapter;
+    TurnosAdapter adapter;
     List<String> turnosReservados;
 
     @Override
@@ -33,88 +28,62 @@ public class MisReservasActivity extends AppCompatActivity {
         turnosReservados = turnosManager.getTurnosReservados();
 
         // Inicializar el adaptador con la lista de turnos reservados
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, turnosReservados);
+        adapter = new TurnosAdapter(this, turnosReservados);
 
         // Establecer el adaptador en la ListView
         listView.setAdapter(adapter);
 
-        // Configurar el OnClickListener para el botón "Dar de Baja"
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        adapter.setOnItemClickListener(new TurnosAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MisReservasActivity.this);
-                builder.setTitle("¿Dar de Baja?")
-                        .setMessage("¿Desea dar de baja este turno?")
-                        .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Eliminar el turno de la base de datos y de la lista
-                                eliminarTurno(position);
-                                // Agregar este bloque
-                                Intent intent = new Intent();
-                                intent.putExtra("turno_dado_de_baja", adapter.getItem(position));
-                                setResult(RESULT_OK, intent);
-                                finish();
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // No hacer nada
-                            }
-                        })
-                        .show();
+            public void onModificarClick(int position) {
+                // Lógica para modificar el turno
+                // Puedes implementar lo que necesites aquí, como abrir una actividad de edición del turno
+                // Por ejemplo:
+                // Intent intent = new Intent(MisReservasActivity.this, EditarTurnoActivity.class);
+                // intent.putExtra("turno", turnosReservados.get(position));
+                // startActivity(intent);
+                Toast.makeText(MisReservasActivity.this, "Modificar turno en posición: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onEliminarClick(int position) {
+                eliminarTurno(position);
             }
         });
 
         // Obtén la referencia al ImageView del icono de retroceso
         ImageView backIconImageView = findViewById(R.id.backIcon);
         // Establece un OnClickListener para el icono de retroceso
-        backIconImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cuando se hace clic en el icono de retroceso, finaliza la actividad actual
-                finish();
-            }
-        });
+        backIconImageView.setOnClickListener(v -> finish());
+
 
         // Obtén la referencia al ImageView del icono home
         ImageView homeIconImageView = findViewById(R.id.home);
         // Establece un OnClickListener para el icono home
-        homeIconImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cuando se hace clic en el icono home, inicia la HomeActivity
-                Intent intent = new Intent(MisReservasActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }
+        homeIconImageView.setOnClickListener(v -> {
+            // Cuando se hace clic en el icono home, inicia la HomeActivity
+            Intent intent = new Intent(MisReservasActivity.this, HomeActivity.class);
+            startActivity(intent);
         });
 
         // Obtén la referencia al ImageView del icono Mis Reservas
         ImageView misReservasIconImage = findViewById(R.id.mis_reservas);
         // Establece un OnClickListener para el icono reservas
-        misReservasIconImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Aquí podrías implementar alguna lógica adicional, como actualizar la lista de reservas
-                Toast.makeText(MisReservasActivity.this, "Mis Reservas", Toast.LENGTH_SHORT).show();
-            }
-        });
+        misReservasIconImage.setOnClickListener(v ->
+                Toast.makeText(MisReservasActivity.this, "Mis Reservas", Toast.LENGTH_SHORT).show()
+        );
 
         // Obtén la referencia al ImageView del icono usuarios
         ImageView userIconImage = findViewById(R.id.user);
         // Establece un OnClickListener para el icono usuarios
-        userIconImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cuando se hace clic en el icono usuarios, inicia la UserActivity
-                Intent intent = new Intent(MisReservasActivity.this, UserActivity.class);
-                startActivity(intent);
-            }
+        userIconImage.setOnClickListener(v -> {
+            // Cuando se hace clic en el icono usuarios, inicia la UserActivity
+            Intent intent = new Intent(MisReservasActivity.this, UserActivity.class);
+            startActivity(intent);
         });
     }
 
-    private void eliminarTurno(int position) {
+    public void eliminarTurno(int position) {
         // Eliminar el turno de la base de datos
         TurnoReservadoDataSource dataSource = new TurnoReservadoDataSource(MisReservasActivity.this);
         try {
--- app/src/main/java/com/example/tortupadel/TurnosAdapter.java
+++ app/src/main/java/com/example/tortupadel/TurnosAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,17 @@ import java.util.List;
 
 public class TurnosAdapter extends ArrayAdapter<String> {
     private List<String> turnos;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onModificarClick(int position);
+
+        void onEliminarClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
 
     public TurnosAdapter(@NonNull Context context, @NonNull List<String> turnos) {
         super(context, 0, turnos);
@@ -22,12 +34,14 @@ public class TurnosAdapter extends ArrayAdapter<String> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         ViewHolder holder;
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.simple_list_item_1, parent, false);
             holder = new ViewHolder();
             holder.textView = convertView.findViewById(android.R.id.text1);
+            holder.modificarButton = convertView.findViewById(R.id.modificarButton);
+            holder.eliminarButton = convertView.findViewById(R.id.darDeBajaButton);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -37,11 +51,30 @@ public class TurnosAdapter extends ArrayAdapter<String> {
 
         holder.textView.setText(turno);
 
+        holder.modificarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.onModificarClick(position);
+                }
+            }
+        });
+
+        holder.eliminarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.onEliminarClick(position);
+                }
+            }
+        });
+
         return convertView;
     }
 
     static class ViewHolder {
         TextView textView;
+        ImageView modificarButton;
+        ImageView eliminarButton;
     }
-
 }

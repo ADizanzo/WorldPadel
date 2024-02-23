7d21944931c323df40c22d7f5b35535eaad7953a
ADizanzo <augustodizanzo@gmail.com>
2024-02-23 20:29:28 -0300

    agrego iconos, actualizo activitys torneos y perfil (funciona)

--- app/src/main/java/com/example/tortupadel/UserActivity.java
+++ app/src/main/java/com/example/tortupadel/UserActivity.java
@@ -1,10 +1,13 @@
 package com.example.tortupadel;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserActivity extends AppCompatActivity {
@@ -26,12 +29,6 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
-
         // Obtén la referencia al ImageView del icono home
         ImageView homeIconImageView = findViewById(R.id.home);
 
@@ -40,11 +37,10 @@ public class UserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Cuando se hace clic en el icono home, inicia la HomeActivity
-                Intent intent = new Intent(UserActivity.this,HomeActivity.class);
+                Intent intent = new Intent(UserActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
-
 
         // Obtén la referencia al ImageView del icono reservas
         ImageView misReservasIconImage = findViewById(R.id.mis_reservas);
@@ -54,7 +50,7 @@ public class UserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Cuando se hace clic en el icono reservas, inicia MisReservasActivity
-                Intent intent = new Intent(UserActivity.this,MisReservasActivity.class);
+                Intent intent = new Intent(UserActivity.this, MisReservasActivity.class);
                 startActivity(intent);
             }
         });
@@ -79,9 +75,80 @@ public class UserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Cuando se hace clic en el icono usuarios, inicia UserActivity
-                Intent intent = new Intent(UserActivity.this,UserActivity.class);
+                Intent intent = new Intent(UserActivity.this, UserActivity.class);
                 startActivity(intent);
             }
         });
+
+
+
+        // Obtén la referencia al EditText de disponibilidad
+        final EditText disponibilidadEditText = findViewById(R.id.disponibilidadEditText);
+
+        // Establece un OnClickListener para el EditText de disponibilidad
+        disponibilidadEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Muestra un cuadro de diálogo con las opciones de disponibilidad
+                showDisponibilidadDialog();
+            }
+        });
+
+        // Obtén la referencia al EditText de categoría
+        final EditText categoriaEditText = findViewById(R.id.categoriaEditText);
+
+        // Establece un OnClickListener para el EditText de categoría
+        categoriaEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Muestra un cuadro de diálogo con las opciones de categoría
+                showCategoriaDialog();
+            }
+        });
     }
+
+    // Método para mostrar el cuadro de diálogo de disponibilidad
+    private void showDisponibilidadDialog() {
+        final String[] opcionesDisponibilidad = {"Mañana", "Mañana/Tarde", "Tarde", "Tarde/Noche", "Noche"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Selecciona Disponibilidad");
+        builder.setItems(opcionesDisponibilidad, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Establecer el texto seleccionado en el EditText de disponibilidad
+                EditText disponibilidadEditText = findViewById(R.id.disponibilidadEditText);
+                disponibilidadEditText.setText(opcionesDisponibilidad[which]);
+
+                // Cerrar el cuadro de diálogo
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    // Método para mostrar el cuadro de diálogo de categoría
+    private void showCategoriaDialog() {
+        final String[] opcionesCategoria = {"8va", "7ma", "6ta", "5ta", "4ta", "3ra", "2da", "1ra"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Selecciona Categoría");
+        builder.setItems(opcionesCategoria, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Establecer el texto seleccionado en el EditText de categoría
+                EditText categoriaEditText = findViewById(R.id.categoriaEditText);
+                categoriaEditText.setText(opcionesCategoria[which]);
+
+                // Cerrar el cuadro de diálogo
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }
+
+
+
 }
